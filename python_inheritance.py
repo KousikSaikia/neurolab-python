96a187b7181b4916e35e420d6458fa2f884d54e9
= <=>
2022-11-23 19:30:06 +0530

    commit

--- python_inheritance.py
+++ python_inheritance.py
@@ -4,18 +4,30 @@ class Person:
         self.name = name
 
     def displayName(self):
-        print(self.name)
+        print(self.name, " is a good boy")
 
     def isEmploeed(self):
         print(self.name, " is not employeed")
 
 class Employee(Person):
+
+    def __init__(self, name,salary,id,designation):
+        
+        self.salary = salary
+        self.id = id
+        self.designation = designation
+
+        #Calling constructor of base class
+        #Person.__init__(self,name)
+        super().__init__(name)
+
     def isEmplyeed(self):
         print(self.name, " is an employee")
 
 emp1=Person('Hari')
-emp=Employee('Kousik')
-emp.displayName()
-emp.isEmplyeed()
 emp1.displayName()
 emp1.isEmploeed()
+
+emp=Employee('Kousik',1000,21,'SVP')
+emp.displayName()
+print(emp.salary)
