75a6ed54b9a61d9b779b21d18c7046fa4d221039
= <=>
2022-11-24 00:46:32 +0530

    commit megaassignment

---
+++ map.py
@@ -0,0 +1,17 @@
+list1=[1,2,3,4,5,6]
+
+lst2 = list(map(lambda x:x*x, list1))
+print(list1)
+print(lst2)
+
+
+print(list(map(lambda a,b:a+b,list1,lst2)))
+
+
+import functools
+
+add_num = lambda x,y:x+y
+print(functools.reduce(add_num, list1, 0))
+
+
+print(list(filter(lambda x:x%2!=0, list1)))
--- megaassignment.py
+++ megaassignment.py
@@ -24,3 +24,66 @@ for x,y in lst1 :
     dict3[x]=y
 
 print(dict3)
+
+#Write a Python program to create a list of tuples from given list having number and its cube in each tuple
+list = [9, 5, 6]
+
+print([(x,x**3) for x in list])
+
+#Write a Python program to get all combinations of 2 tuples.
+
+test_tuple1 = (7, 2)
+test_tuple2 = (7, 8)
+
+result1 = [(x,y) for x in test_tuple1 for y in test_tuple2]
+result2 = [(x,y) for x in test_tuple2 for y in test_tuple1]
+print(result1+result2)
+
+# Write a Python program to sort a list of tuples by second item.
+
+list1= [('for', 24), ('Geeks', 8), ('Geeks', 30)] 
+
+reslt = []
+print(type(reslt))
+for x,y in list1:
+    print(y)
+   # reslt.sort()
+reslt.sort()
+
+
+list1.sort(reverse=False,key=lambda x: x[1])
+print(list1)
+
+
+
+
+for x in range(1,6):
+    print('* '*x)
+
+
+for y in range(1,6):
+    print(((5-y)*' ')+'*'*y)
+
+
+for x in range(1,6):
+    for y in range(1,x+1):
+        print(y, end=" ")
+    print(" ")
+
+
+x = 5
+for i in range(x):
+    for j in range(x):
+        print(end=" ")
+    x = x - 1
+    for j in range(i + 1):
+        print("* ", end=' ')
+    print(" ")
+
+
+list1 = ['A','B','C','D','E']
+
+for x in range(len(list1)):
+    for y in range(x+1):
+        print(list1[x],end=' ')
+    print('')
